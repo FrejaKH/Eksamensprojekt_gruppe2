ffa6da54b0824f976a878f1de13e7ea50787f15c
MortenRUD <morten@mortenrudjensen.dk>
2021-12-01 15:57:36 +0100

    start af insert

---
+++ models/piefitdata.sql
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+INSERT INTO cards (name) VALUES (#PiefitBogota,
+#PiefitHavana,
+#PiefitNassau,
+#PiefitBrasilia,
+#PiefitBangkok,
+#PiefitShanghai,
+#PiefitManama,
+#PiefitBaghdad,
+#PiefitTokyo,
+#PiefitSeoul,
+#PiefitThimphu,
+#PiefitBeijing,
+#PiefitJakarta)
+
+
+
+8	#PiefitHavana	2		amrap	1
+9	#PiefitNassau	3		amrap	1
+10	#PiefitBrasilia	4		amrap	1
+11	#PiefitBangkok	1		workandrest	2
+12	#PiefitShanghai	2		workandrest	2
+13	#PiefitManama	3		workandrest	2
+14	#PiefitBaghdad	4		workandrest	2
+
+(
+(1,	'PIEFITCARDS PACK – CORE HOME EDITION V.1',		2021-11-22,	1,	99,00),
+(2,	'PIEFITCARDS PACK – LOWERBODY HOME EDITION V.1',		2021-11-22,	1,	99,00),
+(3,	'PIEFITCARDS PACK – UPPERBODY HOME EDITION V.1',		2021-11-22,	1,	99,00),
+(4,	'PIEFITCARDS PACK – FULLBODY HOME EDITION V.1',		2021-11-22,	1,	99,00)
+)
