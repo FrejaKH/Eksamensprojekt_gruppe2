c875438cb3c476db11d6675b20354d541b4c56d1
Morten <morten@mortenrudjensen.dk>
2021-12-07 11:21:18 +0100

    databaseconnection

--- models/dbParams.js
+++ models/dbParams.js
@@ -1,8 +1,15 @@
 'use strict';
 
+// module.exports = {
+//     host     : '127.0.0.1',
+//     user     : 'nobody',
+//     password : 'test',
+//     database : 'nodecontacts'
+// }
+
 module.exports = {
     host     : '127.0.0.1',
-    user     : 'nobody',
-    password : 'test',
-    database : 'nodecontacts'
+    user     : 'root',
+    password : 'donotmodifyroot',
+    database : 'piefitdatabase'
 }
