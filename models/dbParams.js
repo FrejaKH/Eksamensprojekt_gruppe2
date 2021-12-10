74717373e56434afaab2fb08f25c0e344f87a716
Morten <morten@mortenrudjensen.dk>
2021-12-10 02:19:01 +0100

    DbParams: Added all our connections

--- models/dbParams.js
+++ models/dbParams.js
@@ -1,15 +1,25 @@
 "use strict";
 
+//DENNIS
 // module.exports = {
-//     host     : '127.0.0.1',
-//     user     : 'nobody',
-//     password : 'test',
-//     database : 'nodecontacts'
-// }
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: "test",
+//   database: "piefitdatabase",
+// };
 
+//FREJA
+// module.exports = {
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: "root",
+//   database: "piefitdatabase",
+// };
+
+//MORTEN
 module.exports = {
   host: "127.0.0.1",
   user: "root",
-  password: "root",
+  password: "donotmodifyroot",
   database: "piefitdatabase",
 };
