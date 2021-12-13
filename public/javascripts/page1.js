77fad8d06865ee204d20afc4845698635dd58345
Morten <morten@mortenrudjensen.dk>
2021-12-13 13:30:14 +0100

    removed unused pages

--- public/javascripts/page1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-import { makeMenu, setTitles, setFooter } from "./nQm.js";
-
-// const doSomething = function () {
-//     makeMenu('menu');
-//     setTitles('Native Node, Sample');
-//     setFooter('nml', 2021);
-// }
-// window.addEventListener('load', doSomething);
